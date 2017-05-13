76eec34e2184f75d98963d6ebee8e5a0c255ce38
chesteraustin <chesteraustin@gmail.com>
2017-05-13 13:10:22 -0700

    search function for OBAMA works

--- search.js
+++ search.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-	console.log("ready");
+    console.log("ready");
+    search();
 })
+
+function search(){
+
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+
+//article search API
+var apiKey = "api-key=010df052716e4cef8459eeb85a014d07&";
+var q = "q=obama"
+
+queryURL = queryURL + apiKey + q
+
+$.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+              console.log(response);
+
+})
+  };
