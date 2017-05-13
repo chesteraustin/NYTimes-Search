8d9698510eafd422189646507ac2a44193832c87
chesteraustin <chesteraustin@gmail.com>
2017-05-13 13:42:39 -0700

    search term

--- search.js
+++ search.js
@@ -1,27 +1,28 @@
 $(document).ready(function(){
-    console.log("ready");
-    search("trump");
+
+	$("#search").on("click",function(){
+		var term = $("#searchTerm").val();
+		search(term);
+	});
 })
 
 function search(term){
+	var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+	//article search API
+	var apiKey = "api-key=010df052716e4cef8459eeb85a014d07&";
+	var q = "q=" + encodeURIComponent(term);
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+	queryURL = queryURL + apiKey + q;
 
-//article search API
-var apiKey = "api-key=010df052716e4cef8459eeb85a014d07&";
-var q = "q=" + encodeURIComponent(term);
-
-queryURL = queryURL + apiKey + q;
-
-$.ajax({
-        url: queryURL,
-        method: "GET"
-      }).done(function(response) {
-              console.log(response);
-	for(i = 0; i < response.docs; i++){
-		var title =  response.docs[i].headline.main;
-		var contenet = reponse.docs[i].web_url;
-		var author = reponse.docs[i].byline.original;
-	}
-})
-  };
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	})
+	.done(function(response) {
+		for(i = 0; i < response.docs; i++){
+			var title =  response.docs[i].headline.main;
+			var contenet = reponse.docs[i].web_url;
+			var author = reponse.docs[i].byline.original;
+		}
+	})
+}
